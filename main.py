c9b4189cae0055e689e920528ab13294e62cc9e4
marcosmarinalopez <marcosmarinalopez@outlook.es>
2023-05-02 13:24:15 +0200

    local API working

--- main.py
+++ main.py
@@ -1,10 +1,36 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import pickle
 
 app = Flask(__name__)
 
-@app.route('/predict', methods=['GET'])
+@app.route('/', methods=['GET'])
+def home():
+    return """
+    <h1>APP para calcular las ventas a partir de los gastos de marketing</h1>    
+    """
+
+@app.route('/api/v1/predict', methods=['GET'])
 def predict():
-    return "Predicción aquí"
+    predicted_sales = -1
+
+    try:        
+        tv = float(request.args['TV'])
+        radio = float(request.args['radio'])
+        newspaper =  float(request.args['newspaper'])
+        #sales = float(request.args['sales'])
+
+        model_advertising= pickle.load(open('model.pkl', 'rb'))
+
+        data_for_prediction = [tv, radio, newspaper]
+
+        prediction = model_advertising.predict([data_for_prediction])
+        predicted_sales = prediction[0]
+
+    except Exception as ex:
+        predicted_sales = "Bad parameters given. " + str(ex)
+    
+    finally:
+        return jsonify({'Predicted Sales: ':predicted_sales})
 
 if __name__ == '__main__':
-    app.run(port=5000)
+    app.run(debug = True, port=5000)
